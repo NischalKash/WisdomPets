84cae2006d132ea4f8dd5a00a2a13da05f23ac97
NischalKash <nischalkashyap.adm2019@gmail.com>
2020-05-18 12:37:32 -0400

    final

---
+++ wisdompets/wisdompets/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.contrib import admin
+from adoptions import views
+
+urlpatterns = [
+    url('admin/', admin.site.urls),
+    url(r'^$',views.home,name='home'),
+    url(r'^adoptions/(\d+)/',views.pet_detail,name = 'pet_detail'),
+]
